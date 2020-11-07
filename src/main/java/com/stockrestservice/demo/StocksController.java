ce327a71feac1dfd2ccd6462e58b0ce79b6ffa01
Tania <taniaribeiroeng@gmail.com>
2020-11-07 13:24:36 -0300

    bugfix for timezone in sql

---
+++ src/main/java/com/stockrestservice/demo/StocksController.java
@@ -0,0 +1,32 @@
+package com.stockrestservice.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class StocksController {
+
+    @Autowired
+    private StocksRepository stockRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewStock (@RequestParam String name) {
+
+        Stocks n = new Stocks();
+        n.setContent(name);
+        stockRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Stocks> getAllStocks() {
+        // This returns a JSON or XML with the users
+        return stockRepository.findAll();
+    }
+}
